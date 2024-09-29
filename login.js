50271ec51026ba1505e50f8f6da260bcb04646fa
sheninna <sheninnajexellcaisip@gmail.com>
2024-09-29 16:11:28 +0800

    CMS

--- login.js
+++ login.js
@@ -29,3 +29,19 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         event.stopPropagation();
     }
 }, false);
+
+
+// Get the password input field and the toggle icon
+const passwordInput = document.getElementById('password');
+const togglePassword = document.getElementById('togglePassword');
+
+// Add an event listener to the toggle icon
+togglePassword.addEventListener('click', () => {
+  // Toggle the type attribute of the password input field
+  const type = passwordInput.type === 'password' ? 'text' : 'password';
+  passwordInput.type = type;
+
+  // Change the icon based on the password input type
+  const iconClass = type === 'password' ? 'bi bi-eye-slash' : 'bi bi-eye';
+  togglePassword.innerHTML = `<i class="${iconClass}"></i>`;
+});
