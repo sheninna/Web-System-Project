10f1d34ad3266450d59f8b123a89c5a2c89c1d79
KennJ78 <kennbayungan@gmail.com>
2024-11-12 20:45:23 +0800

    updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
-const port = 3050;
+const port = 3055;
 
 const app = express();
 
